ac4c6b5837f9b980e0ff0870a03ce93ca82ab7c5
dkdkdsa <bos94330@gmail.com>
2023-03-30 11:54:49 +0900

    PlayerInput

--- Assets/04_Script/Player/System/PlayerInput.cs
+++ Assets/04_Script/Player/System/PlayerInput.cs
@@ -1,10 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    
 
+    private float moveValue;
+
+    public event Action<float> OnMovementEvent;
+    public event Action OnJumpEvent;
+    public event Action OnBounceEvent;
+    public event Action OnHideEvnet;
+    public event Action OnSkillEvent;
+
+    private void Update()
+    {
+
+        OnMovementEvent?.Invoke(moveValue);
+
+    }
+
+    public void SetMoveValue(float v)
+    {
+
+        moveValue = v;
+
+    }
+
+    public void JumpExecute()
+    {
+
+        OnJumpEvent?.Invoke();
+
+    }
+
+    public void BounceExecute()
+    {
+
+        OnBounceEvent?.Invoke();
+
+    }
+
+    public void HideExecute()
+    {
+
+        OnHideEvnet?.Invoke();
+
+    }
+
+    public void SkillExecute()
+    {
+
+        OnSkillEvent?.Invoke();
+
+    }
 
 }
