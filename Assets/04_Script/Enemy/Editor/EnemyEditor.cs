1067c7b44d639d51d59593faa70d31d3548a56a4
dkdkdsa <bos94330@gmail.com>
2023-03-31 08:51:22 +0900

    CreateEnemy

--- Assets/04_Script/Enemy/Editor/EnemyEditor.cs
+++ Assets/04_Script/Enemy/Editor/EnemyEditor.cs
@@ -7,10 +7,26 @@ public class EnemyEditor : MonoBehaviour
 
     [SerializeField] private string enemyName;
 
+    private GameObject mainObject;
+
     public void CreateEnemy()
     {
 
-        var mainObject = CreateObject(enemyName);
+        if (enemyName == "")
+        {
+
+            Debug.LogError("enemyName이 잘못됨");
+            return;
+
+        }
+
+        #region 메인 오브젝트 생성
+
+        mainObject = CreateObject(enemyName);
+        mainObject.AddComponent<SpriteRenderer>();
+        mainObject.AddComponent<Animator>();
+
+        #endregion
 
     }
 
