0284d16e8c1f4dad5c248b41eb7952285b2c5084
dkdkdsa <bos94330@gmail.com>
2023-03-30 14:58:41 +0900

    InputObject 구조체 추가

--- Assets/04_Script/System/Structs.cs
+++ Assets/04_Script/System/Structs.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Struct
 {
 
-    
+    [System.Serializable]
+    public class InputObject
+    {
+
+        public UnityEvent inputEvent;
+        public KeyCode keyCode;
+
+    }
 
 }
