94e1f800c992a43c6bf65c1da2a929ed297b6017
dkdkdsa <bos94330@gmail.com>
2023-03-30 21:33:24 +0900

    해시값 지정

---
+++ Assets/04_Script/Player/Anime/PlayerAnimator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : PlayerRoot
+{
+
+    private readonly int IsGroundHash = Animator.StringToHash("IsGround");
+    private readonly int IsMoveHash = Animator.StringToHash("IsMove");
+    private readonly int YVelHash = Animator.StringToHash("YVel");
+
+    private void Update()
+    {
+        
+
+
+    }
+
+
+}
