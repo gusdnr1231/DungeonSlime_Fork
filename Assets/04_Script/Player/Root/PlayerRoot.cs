c60482f7e029ff63bc2a5cfa1f29c18a8b68918e
dkdkdsa <bos94330@gmail.com>
2023-03-30 15:25:53 +0900

    PlayerRoot 변수 추가

--- Assets/04_Script/Player/Root/PlayerRoot.cs
+++ Assets/04_Script/Player/Root/PlayerRoot.cs
@@ -8,12 +8,17 @@ public class PlayerRoot : MonoBehaviour
     protected PlayerInput input;
     protected Rigidbody2D rigid;
     protected GroundCol groundCol;
+    protected SpriteRenderer spriteRenderer;
+    protected Animator animator;
 
     protected virtual void Awake()
     {
 
         input = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
         groundCol = FindObjectOfType<GroundCol>();
 
     }
