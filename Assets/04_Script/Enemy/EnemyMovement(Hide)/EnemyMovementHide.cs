cefb184bf403b68e7de46392b3f9007a040065b2
dkdkdsa <98935315+dkdkdsa@users.noreply.github.com>
2023-03-31 18:24:52 +0900

    EnemyData필드 추가

--- Assets/04_Script/Enemy/EnemyMovement(Hide)/EnemyMovementHide.cs
+++ Assets/04_Script/Enemy/EnemyMovement(Hide)/EnemyMovementHide.cs
@@ -1,10 +1,11 @@
+using Struct;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovementHide : EnemyRoot
 {
-    
 
+    [SerializeField] private EnemyData enemyData;
 
 }
