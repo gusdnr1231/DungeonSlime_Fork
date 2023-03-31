2f5def358c3968dbb6e8c69ec704c20887492500
dkdkdsa <98935315+dkdkdsa@users.noreply.github.com>
2023-03-31 18:05:14 +0900

    Awake 정의

--- Assets/04_Script/Enemy/Root/EnemyRoot.cs
+++ Assets/04_Script/Enemy/Root/EnemyRoot.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class EnemyRoot : MonoBehaviour
 {
-    
 
+    protected SpriteRenderer spriteRenderer;
+    protected Animator animator;
+    protected Rigidbody2D rigid;
+
+    protected virtual void Awake()
+    {
+
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
 
 }
