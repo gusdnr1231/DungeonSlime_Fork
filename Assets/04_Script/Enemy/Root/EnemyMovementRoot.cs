15c609dbbb56bc611afe071c6f0506cc85d7fbb1
dkdkdsa <98935315+dkdkdsa@users.noreply.github.com>
2023-03-31 18:15:32 +0900

    EnemyMovementRoot

---
+++ Assets/04_Script/Enemy/Root/EnemyMovementRoot.cs
@@ -0,0 +1,17 @@
+using Interface;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementRoot : EnemyRoot, IEventObject
+{
+    public virtual void AddEvent()
+    {
+
+    }
+
+    public virtual void RemoveEvent()
+    {
+
+    }
+}
