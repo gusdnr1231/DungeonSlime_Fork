30c8ec207f1ceceb13cb7808eaedeb97a7115093
dkdkdsa <98935315+dkdkdsa@users.noreply.github.com>
2023-04-02 03:45:35 +0900

    BoxEnemy

--- Assets/04_Script/Enemy/EnemyScript/Box/BoxEnemy.cs
+++ Assets/04_Script/Enemy/EnemyScript/Box/BoxEnemy.cs
@@ -4,7 +4,42 @@ using UnityEngine;
 
 public class BoxEnemy : EnemyRoot
 {
-    
-    
+
+    [SerializeField] private int maxUpCount = 4;
+
+    private int upCount = 1;
+
+    public override void AddEvent()
+    {
+
+        
+
+    }
+
+    public override void RemoveEvent() 
+    {
+        
+
+
+    }
+
+    private void BoxUpSkill()
+    {
+
+        if (upCount == maxUpCount) return;
+
+        spriteRenderer.size += Vector2.up;
+        enemyCollider.size += Vector2.up;
+        enemyCollider.offset += new Vector2(0, 0.5f);
+        upCount++;
+
+    }
+
+    private void BoxDownSkill()
+    {
+
+
+
+    }
 
 }
