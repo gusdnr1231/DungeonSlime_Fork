542351200539fe0750b672d93fad789c81ea459d
dkdkdsa <98935315+dkdkdsa@users.noreply.github.com>
2023-04-02 03:04:29 +0900

    PlayerHide

--- Assets/04_Script/Player/Event/PlayerHide.cs
+++ Assets/04_Script/Player/Event/PlayerHide.cs
@@ -1,10 +1,14 @@
 using Interface;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerHide : PlayerRoot
 {
+
+    [SerializeField] private Vector2 boxRange;
+    [SerializeField] private LayerMask enemyLayer;
 
     private List<IEventObject> playerEvent = new List<IEventObject>();
 
@@ -15,18 +19,39 @@ public class PlayerHide : PlayerRoot
         input.OnHideEvnet += Hide;
         input.OnBounceEvent += Bounce;
 
+        playerEvent = GetComponents<IEventObject>().ToList();
+
     }
 
     private void Hide()
     {
 
+        RaycastHit2D hitAble = Physics2D.BoxCast(transform.position, boxRange, 0, Vector2.zero, 0, enemyLayer);
 
+        if (hitAble == false) return;
+
+        foreach(var x in playerEvent)
+        {
+
+            x.RemoveEvent();
+
+        }
+
+        spriteRenderer.enabled = false;
 
     }
 
     private void Bounce()
     {
 
+        foreach (var x in playerEvent)
+        {
+
+            x.AddEvent();
+
+        }
+
+        spriteRenderer.enabled = true;
 
     }
 
