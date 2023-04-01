cd41eb9bb751feda0112a8435267e5409b8a04b8
dkdkdsa <98935315+dkdkdsa@users.noreply.github.com>
2023-04-01 22:36:55 +0900

    PlayerMovementRoot상속

--- Assets/04_Script/Player/Event/PlayerHide.cs
+++ Assets/04_Script/Player/Event/PlayerHide.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHide : PlayerRoot
+public class PlayerHide : PlayerMovementRoot
 {
-    
 
+    
 
 }
