88db42773d8ba267f39b345a81dfa503c3fcc450
dkdkdsa <bos94330@gmail.com>
2023-04-03 00:53:26 +0900

    TreeEnemy

---
+++ Assets/04_Script/Enemy/EnemyScript/Tree/TreeEnemy.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeEnemy : EnemyRoot
+{
+
+    public override void AddEvent()
+    {
+        
+
+
+    }
+
+    public override void RemoveEvent() 
+    {
+
+
+
+    }
+
+}
