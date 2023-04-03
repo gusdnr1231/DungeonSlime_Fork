99dc1f9db86f72371ae06e7c9ee09b3d2f782f59
dkdkdsa <bos94330@gmail.com>
2023-04-03 12:10:02 +0900

    CreateDefaultMap

--- Assets/04_Script/Map/Editors/MapEditor.cs
+++ Assets/04_Script/Map/Editors/MapEditor.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapEditor : MonoBehaviour
 {
@@ -16,6 +17,15 @@ public class MapEditor : MonoBehaviour
         #region 디폴트 맵 생성
 
         GameObject mainMap = CreateObject(mapNumber.ToString());
+        mainMap.AddComponent<Grid>();
+
+        #endregion
+
+        #region 타일맵 오브젝트 생성
+
+        GameObject defaultTilemapObject = CreateObject("TileMap");
+        defaultTilemapObject.AddComponent<Tilemap>();
+        defaultTilemapObject.AddComponent<TilemapRenderer>();
 
         #endregion
 
