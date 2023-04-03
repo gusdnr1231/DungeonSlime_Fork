e907404a7a4a0f56b4711ab3fc004bc7b0c0635e
dkdkdsa <bos94330@gmail.com>
2023-04-03 12:03:06 +0900

    CreateDefaultMap

--- Assets/04_Script/Map/Editors/MapEditor.cs
+++ Assets/04_Script/Map/Editors/MapEditor.cs
@@ -5,7 +5,22 @@ using UnityEngine;
 
 public class MapEditor : MonoBehaviour
 {
-    
+
+    [SerializeField] private int mapNumber;
+
+    public void CreateDefaultMap()
+    {
+
+        if (mapNumber <= 0) return;
+
+        #region 디폴트 맵 생성
+
+        GameObject mainMap = CreateObject(mapNumber.ToString());
+
+        #endregion
+
+    }
+
     private GameObject CreateObject(string name = "Obj")
     {
 
