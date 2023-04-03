c13dbb63be180bd78a2ea35e54e06ef65383c88c
dkdkdsa <bos94330@gmail.com>
2023-04-03 18:02:31 +0900

    DieSensor완성

--- Assets/04_Script/Player/System/DieSensor.cs
+++ Assets/04_Script/Player/System/DieSensor.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DieSensor : MonoBehaviour
 {
 
     [SerializeField] private List<string> tags = new List<string>();
+    [SerializeField] private UnityEvent dieEvent;
+
+    private bool isDie;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,7 +20,10 @@ public class DieSensor : MonoBehaviour
             if (collision.CompareTag(tag))
             {
 
-                Debug.Log(1);
+                if (isDie) return;
+
+                isDie = true;
+                dieEvent?.Invoke();
 
             }
 
