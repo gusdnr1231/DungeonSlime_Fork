821c5e7eabaf0c4b2a400da25cf526f213cc25a5
dkdkdsa <bos94330@gmail.com>
2023-04-03 12:20:26 +0900

    MapEditorUI

--- Assets/04_Script/Map/Editors/MapEditorUI.cs
+++ Assets/04_Script/Map/Editors/MapEditorUI.cs
@@ -1,18 +1,13 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class MapEditorUI : MonoBehaviour
+public class MapEditorUI : Editor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
+#endif
