802f951f54736b56f71825ea9ac7381ac6bd0e22
dkdkdsa <bos94330@gmail.com>
2023-04-03 14:52:29 +0900

    sensor

---
+++ Assets/04_Script/Player/System/DieSensor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieSensor : MonoBehaviour
+{
+
+    [SerializeField] private List<string> tags = new List<string>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+
+    }
+
+}
