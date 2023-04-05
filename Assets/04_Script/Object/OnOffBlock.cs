8bf8e47446e903b796b46e3ba19fba6ad39d7db4
dkdkdsa <bos94330@gmail.com>
2023-04-06 08:55:36 +0900

    Make OnOffBlock Anime

--- Assets/04_Script/Object/OnOffBlock.cs
+++ Assets/04_Script/Object/OnOffBlock.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Rendering;
@@ -14,7 +15,13 @@ public class OnOffBlock : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
 
-        ChangeOnOff();
+        if(collision.transform.position.y > transform.position.y)
+        {
+
+            ChangeOnOff();
+
+        }
+
 
     }
 
