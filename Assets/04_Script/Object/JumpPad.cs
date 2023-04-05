213d86cd85932f40923e89b01b9bc1e1b39011f4
dkdkdsa <bos94330@gmail.com>
2023-04-06 00:32:19 +0900

    OnTriggerEnter2D

--- Assets/04_Script/Object/JumpPad.cs
+++ Assets/04_Script/Object/JumpPad.cs
@@ -6,11 +6,33 @@ public class JumpPad : MonoBehaviour
 {
 
     [SerializeField] private List<string> jumpAbleTag = new List<string>();
+    [SerializeField] private Vector2 posVector;
+    [SerializeField] private float bouncePower;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
-        i
+        foreach(var tag in jumpAbleTag)
+        {
+
+            if(collision.CompareTag(tag) && collision.TryGetComponent<PlayerMove>(out var pMove))
+            {
+
+                pMove.moveAble = false;
+                pMove.GetComponent<Rigidbody2D>().velocity += posVector * bouncePower;
+                pMove.SetValMoveAble();
+
+            }
+            else if (collision.CompareTag(tag) && collision.TryGetComponent<EnemyMovementHide>(out var eMove))
+            {
+
+                eMove.moveAble = false;
+                eMove.GetComponent<Rigidbody2D>().velocity += posVector * bouncePower;
+                eMove.SetValMoveAble();
+
+            }
+
+        }
 
     }
 
