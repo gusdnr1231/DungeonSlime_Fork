4ef2a5067024ed63208d941cb508d6a030f0859e
dkdkdsa <bos94330@gmail.com>
2023-04-07 01:23:03 +0900

    Pipe

--- Assets/04_Script/Object/Pipe.cs
+++ Assets/04_Script/Object/Pipe.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using FD.Dev;
 using DG.Tweening;
+using Interface;
 
 public class Pipe : MonoBehaviour
 {
-    
 
+    [SerializeField] private Transform outputPipe;
+
+    private void UsePipe(Transform useObjcet)
+    {
+
+        
+
+    }
 
 }
