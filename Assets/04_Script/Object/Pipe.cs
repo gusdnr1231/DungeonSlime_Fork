ff10a593e32b8fef792d1f4503c472e3c46fc6fd
dkdkdsa <bos94330@gmail.com>
2023-04-07 01:11:03 +0900

    Pipe 재정의

--- Assets/04_Script/Object/Pipe.cs
+++ Assets/04_Script/Object/Pipe.cs
@@ -4,48 +4,9 @@ using UnityEngine;
 using FD.Dev;
 using DG.Tweening;
 
-public class Pipe : EnemyRoot
+public class Pipe : MonoBehaviour
 {
-    [SerializeField] private Transform otherPipe;
-    private GameObject playerTrs;
+    
 
-    EnemyMovementHide movementHide;
 
-    protected override void Awake()
-    {
-
-        base.Awake();
-        movementHide = gameObject.GetComponent<EnemyMovementHide>();
-        playerTrs = GameObject.FindWithTag("Player");
-    }
-
-    void Spon()
-    {
-        Debug.Log(383);
-        movementHide.SetValMoveAble();
-
-        playerTrs.GetComponent<SpriteRenderer>().enabled = true;
-
-        float up = playerTrs.transform.position.y - transform.position.y;
-
-        playerTrs.transform.DOMoveY(transform.position.y, 1);
-        playerTrs.transform.position = otherPipe.position;
-        playerTrs.transform.DOMoveY(otherPipe.position.y + up, 1);
-
-        input.BounceExecute();
-    }
-
-    public override void AddEvent()
-    {
-
-        input.OnHideEvnet += Spon;
-
-    }
-
-    public override void RemoveEvent()
-    {
-
-        input.OnHideEvnet -= Spon;
-
-    }
 }
