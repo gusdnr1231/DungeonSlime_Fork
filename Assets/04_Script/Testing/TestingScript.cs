c5555ba1bca361fb96914e73dabb7eac4a55fa47
dkdkdsa <bos94330@gmail.com>
2023-04-11 23:20:31 +0900

    TestLog

--- Assets/04_Script/Testing/TestingScript.cs
+++ Assets/04_Script/Testing/TestingScript.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class TestingScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void TestLog()
     {
-        
+
+        Debug.Log("TestingLog!");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
