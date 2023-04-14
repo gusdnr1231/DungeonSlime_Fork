11ea5ce479731ccb6eeaef4b3828cb34752744a7
JangWheesung <gameplay1qaz@gmail.com>
2023-04-14 22:06:33 +0900

    Scale1

---
+++ Assets/04_Script/Object/Scale.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    [SerializeField] private GameObject scale1, scale2;
+    [SerializeField] private GameObject stick;
+    [SerializeField] private Vector2 overlapBoxSize;
+
+    private Transform s1T, s2T;
+
+    private void Awake()
+    {
+        s1T = scale1.transform;
+        s2T = scale2.transform;
+    }
+
+    void Update()
+    {
+        scale1.transform.position = s1T.position;
+        scale2.transform.position = s2T.position;
+
+        SetStick(Angle());
+        SetBalanceStones(Angle());
+    }
+
+    void SetStick(float angle)
+    {
+        float position1Y = Mathf.Lerp(s1T.position.y, s1T.position.y + angle, Time.deltaTime);
+        float position2Y = Mathf.Lerp(s2T.position.y, s2T.position.y + angle, Time.deltaTime);
+
+        s1T.position = new Vector3(0, position1Y);
+        s2T.position = new Vector3(0, position2Y);
+    }
+
+    void SetBalanceStones(float angle)
+    {
+        float eulerZ = Mathf.Lerp(stick.transform.rotation.z, angle, Time.deltaTime);
+        stick.transform.rotation = Quaternion.Euler(0, 0, eulerZ);
+    }
+
+    float Angle()
+    {
+        return HeavySenser(scale1) - HeavySenser(scale2);
+    }
+
+    float HeavySenser(GameObject scale)
+    {
+        RaycastHit2D[] d = Physics2D.BoxCastAll(scale.transform.position,
+            overlapBoxSize, 0, Vector2.zero);
+        
+        Heavy[] hs = new Heavy[20];
+        for (int i = 0; i < d.Length; i++)
+        {
+            hs[i] = d[i].transform.GetComponent<Heavy>();
+        }
+
+        float sumHeavy = 0;
+        foreach (Heavy heavy in hs)
+        {
+            sumHeavy += heavy.heavy;
+        }
+
+        return sumHeavy;
+    }
+}
