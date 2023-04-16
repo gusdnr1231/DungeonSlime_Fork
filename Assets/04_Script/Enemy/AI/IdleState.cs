0a8a75198a9cc9e36a7ed9d220911205dc568ac0
dkdkdsa <bos94330@gmail.com>
2023-04-16 23:58:09 +0900

    IdleState

---
+++ Assets/04_Script/Enemy/AI/IdleState.cs
@@ -0,0 +1,22 @@
+using FD.AI.FSM;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : FAED_FSMState
+{
+    public override void EnterState()
+    {
+
+    }
+
+    public override void ExitState()
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+        
+    }
+}
