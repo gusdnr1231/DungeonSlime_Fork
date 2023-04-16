d03640615f0d2a9a71cbf7a4e8febb8ed1f610e4
dkdkdsa <bos94330@gmail.com>
2023-04-17 00:46:49 +0900

    ChaceState

--- Assets/04_Script/Enemy/AI/ChaceState.cs
+++ Assets/04_Script/Enemy/AI/ChaceState.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FD.AI.FSM;
+using DG.Tweening;
 
-public class ChaceState : MonoBehaviour
+public class ChaceState : FAED_FSMState
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Rigidbody2D rigid;
+    private Transform player;
+
+    private void Awake()
     {
-        
+
+        player = GameObject.Find("Player").transform;
+        rigid = transform.parent.GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void EnterState()
+    {
+
+
+    }
+
+    public override void UpdateState()
     {
         
+        float value = player.position.x > transform.position.x ? -3f : 3f;
+
+        rigid.velocity = new Vector2(value, rigid.velocity.y);
+
     }
+
+    public override void ExitState()
+    {
+        
+        rigid.velocity = Vector2.zero;
+
+    }
+
 }
