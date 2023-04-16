d9ca0f8c86b67b647695d231f63c898cc721fd48
dkdkdsa <bos94330@gmail.com>
2023-04-17 00:06:40 +0900

    TryChageChace

--- Assets/04_Script/Enemy/AI/TryChageChace.cs
+++ Assets/04_Script/Enemy/AI/TryChageChace.cs
@@ -8,10 +8,21 @@ public class TryChageChace : FAED_FSMTransition
 
     [SerializeField] private float maxRange;
 
+    private Transform player;
+
+    private void Awake()
+    {
+
+        player = GameObject.Find("Player").transform;
+
+    }
+
     public override bool ChackTransition()
     {
 
-        return false;
+        bool value = (Mathf.Abs(transform.position.x) - Mathf.Abs(player.position.x)) <= maxRange;
+
+        return value;
 
     }
 
@@ -20,7 +31,12 @@ public class TryChageChace : FAED_FSMTransition
     private void OnDrawGizmos()
     {
         
+        Color old = Gizmos.color;
+        Gizmos.color = Color.white;
+        //DrawCode
+        Gizmos.DrawWireCube(transform.position, new Vector2(maxRange * 2, 1));
 
+        Gizmos.color = old;
 
     }
 
