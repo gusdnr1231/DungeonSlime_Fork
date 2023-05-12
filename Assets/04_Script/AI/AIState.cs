a3332f35bbdb7cc11308ddaac914a6badc21baad
dkdkdsa <bos94330@gmail.com>
2023-05-13 00:06:49 +0900

    AIState

--- Assets/04_Script/AI/AIState.cs
+++ Assets/04_Script/AI/AIState.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 
 public abstract class AIState : IAIState
 {
+
+    protected AIController controller;
     public abstract void EnterState();
     public abstract void UpdateState();
     public abstract void ExitState();
     public virtual void SettingState(AIController controller)
     {
 
-
+        this.controller = controller;
 
     }
 
