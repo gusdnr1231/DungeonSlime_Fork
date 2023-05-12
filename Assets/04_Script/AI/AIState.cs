566e5621f8df7f1bd15a030407610bd2089dedb9
dkdkdsa <bos94330@gmail.com>
2023-05-13 00:05:19 +0900

    AIState

--- Assets/04_Script/AI/AIState.cs
+++ Assets/04_Script/AI/AIState.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIState : IAIState
+public abstract class AIState : IAIState
 {
     public void EnterState()
     {
