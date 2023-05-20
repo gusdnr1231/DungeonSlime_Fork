b1a2bfeceff7677625f5abb77f275fdba90a6ad6
dkdkdsa <bos94330@gmail.com>
2023-05-20 19:58:20 +0900

    SkellAnimator

--- Assets/04_Script/Enemy/EnemyScript/Skell/SkellAnimator.cs
+++ Assets/04_Script/Enemy/EnemyScript/Skell/SkellAnimator.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class SkellAnimator : MonoBehaviour
 {
-    
-    //Hash
 
+    //Hash
+    private readonly int IsMoveHash = Animator.StringToHash("IsMove");
+    private readonly int IsAirHash = Animator.StringToHash("IsAir");
+    private readonly int JumpTriggerHash = Animator.StringToHash("JumpTrigger");
+    private readonly int PaintHash = Animator.StringToHash("Paint");
+    private readonly int ReleasePaintHash = Animator.StringToHash("ReleasePaint");
     //
 
     private Animator animator;
