e4a0000faf2cc72f1ffd1042d4f9c48e40246e0f
dkdkdsa <bos94330@gmail.com>
2023-05-21 22:48:53 +0900

    InvisibleWall

---
+++ Assets/04_Script/Object/InvisibleWall.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWall : MonoBehaviour
+{
+
+    private string[] strTag;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+
+
+    }
+
+}
