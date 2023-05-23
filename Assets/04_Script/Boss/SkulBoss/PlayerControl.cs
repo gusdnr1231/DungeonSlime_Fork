b6ef382cb8ef9394601fa26a8263d61548c5a6ed
dkdkdsa <bos94330@gmail.com>
2023-05-23 22:20:38 +0900

    SkulBossRealClear

--- Assets/04_Script/Boss/SkulBoss/PlayerControl.cs
+++ Assets/04_Script/Boss/SkulBoss/PlayerControl.cs
@@ -7,32 +7,35 @@ using UnityEngine;
 public class PlayerControl : FAED_FSMState
 {
     [Header("Object")]
-    [SerializeField] private GameObject player;
     [SerializeField] private GameObject skullBoss;
     [SerializeField] private Transform popPoint;
     [Header("Other")]
-    [SerializeField] private CinemachineVirtualCamera cv;
     [SerializeField] private Sprite leftSprite;
     [SerializeField] private Sprite rightSprite;
     [SerializeField] float moveSpeed;
 
+    private GameObject player;
     private SkulBossAppear bossAppear;
     private Rigidbody2D skbRg;
     private SpriteRenderer skbSp;
     private Animator skbAnim;
+    private CinemachineVirtualCamera cv;
 
     bool hit;
 
     void Awake()
     {
+        player = FindObjectOfType<PlayerAnimator>().gameObject;
         bossAppear = FindObjectOfType<SkulBossAppear>();
         skbRg = skullBoss.GetComponent<Rigidbody2D>();
         skbSp = skullBoss.GetComponent<SpriteRenderer>();
         skbAnim = skullBoss.GetComponent<Animator>();
+        cv = FindObjectOfType<CinemachineVirtualCamera>();
     }
 
     public override void EnterState()
     {
+        Debug.Log("¾¾¹ß¼½½ºdd");
         player.SetActive(false);
         skbRg.constraints &= ~RigidbodyConstraints2D.FreezePosition;
         cv.Follow = skullBoss.transform;
@@ -42,11 +45,13 @@ public class PlayerControl : FAED_FSMState
 
     public override void ExitState()
     {
+        Debug.Log("¾¾¹ß¼½½ºgg");
         StopAllCoroutines();
     }
 
     public override void UpdateState()
     {
+        Debug.Log("¾¾¹ß¼½½º");
         if (!hit) BossMove();
     }
 
@@ -56,7 +61,7 @@ public class PlayerControl : FAED_FSMState
         float inputY = Input.GetAxisRaw("Vertical");
 
         if (Input.GetKey(KeyCode.W)) inputY = 1;
-        if (Input.GetKey(KeyCode.Space)) inputY = 0;
+        if (Input.GetKey(KeyCode.S)) inputY = -1;
 
         skbRg.velocity = new Vector2(inputX, inputY) * moveSpeed;
 
