dec114bf5abdf805b9bc592b8364db60c83895f1
dkdkdsa <bos94330@gmail.com>
2023-05-27 20:18:55 +0900

    부활

--- Assets/04_Script/Object/GolemDieBlock.cs
+++ Assets/04_Script/Object/GolemDieBlock.cs
@@ -1,3 +1,4 @@
+using FD.Dev;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,10 +6,36 @@ using UnityEngine;
 
 public class GolemDieBlock : MonoBehaviour
 {
+
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        
+        animator = GetComponent<Animator>();
+
+    }
+
     public void Setting()
     {
 
+        FAED.InvokeDelayReal(() =>
+        {
 
+            animator.SetTrigger("Set");
+
+        }, 8f);
 
     }
+
+
+    public void Summon()
+    {
+
+        Instantiate(Resources.Load("Enemy/Golem"), transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+
+    }
+
 }
